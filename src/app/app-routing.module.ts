14a457b322edea438aebcf72cafb6045f3b35cff
Mahendra Singamsetty <mahendrasv@gmail.com>
2020-02-19 10:00:27 +0530

    Routing is implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProductComponent } from './product/product.component';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent
+  },
+  {
+    path: 'todos',
+    component: TodoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
